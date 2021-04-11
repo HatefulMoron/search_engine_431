83416c8052542eff5e9d7160ba99dd84f9d801cb
Thomas Passmore <thomas@hateful.dev>
2021-04-11 23:34:25 +1200

    Buffer the search engine output
    The test 50 queries, when run in series, takes less than a second in
    total, which is a good starting point.
    
    Signed-off-by: Thomas Passmore <thomas@hateful.dev>

--- src/search.rs
+++ src/search.rs
@@ -4,18 +4,22 @@ use indexing::index::DiskIndex;
 use parsing::terms::Terms;
 
 use std::fs::File;
-use std::io::{stdin, BufRead, BufReader, Cursor, Read};
+use std::io::{stdin, stdout, BufRead, BufReader, Cursor, Read, BufWriter, Write};
 
 fn main() -> std::io::Result<()> {
     let mut index = DiskIndex::from_disk()?;
+
+    let stdout = stdout();
+    let mut out = BufWriter::new(stdout.lock());
 
     let stdin = stdin();
     for line in stdin.lock().lines() {
         if let Ok(str) = line {
             let results = index.search(&str)?;
             for r in results {
-                println!("{} {}", r.0, r.1);
+                writeln!(out, "{} {}", r.0, r.1)?;
             }
+            out.flush()?;
         }
     }
     //        println!("query: '{}'", str);
